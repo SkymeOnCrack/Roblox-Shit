560bae2913a00ddcf93f1b39fb3079d0ac89904a
LOOF-sys <59900863+LOOF-sys@users.noreply.github.com>
2021-11-23 12:24:17 -0600

    Add files via upload

---
+++ dump[obfuscator breaker].lua
@@ -0,0 +1,168 @@
+print("string")
+function get()
+   return "C++ GOT ME"
+end
+shit = "no"
+shitter = 1
+asser = warn
+butt = {cute = "cute"}
+numarray = {1,2,3}
+arrays = {{},{},{}}
+arraysbutdefined = {e = {},r = {},t = {}}
+stringarray = {"amogus","sus",amogus = "sus",sus = "amogus"}
+numberdefarray = {a = 1;b = 2;c = 3}
+innerarrays = {{a = 1;b = "s";c = {}},l = {e = "r",p = 1;f= {}}}
+warn("reee")
+-- challenge variables
+local pcall = pcall
+local getfenv = getfenv
+local stor = getfenv()
+-- -- -- -- -- -- -- --
+pcall(function()
+    error("get this??")
+end)
+suck_my_dick = {
+    [1] = "no";
+    [2] = "thank";
+    [3] = "you";
+    [4] = 4;
+    [5] = "INT";
+    [6] = "THIS WILL FUCK YOU";
+    [7] = {{[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {}}},{[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {}}},{[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {[1] = "e";[2] = o;[3] = nil;[4] = arrays[1];[5] = self;[6] = self;[7] = self;[8] = "self... NOT LOL";[9] = {}}}}
+}
+self_or_not = {
+    [1] = "stop";
+    [2] = "cringe";
+    [3] = 3;
+    [4] = "dont do this just do {1,2,3,4}";
+    [5] = "no need for fucking {[1] = \"shit\"} ";
+    [6] = "this will throw off the obfuscator";
+    [7] = {[1] = {[1] = {[1] = {[1] = {[1] = {[2] = {[2] = {[3] = suck_my_dick[7][1][9][9];[4] = self}}}}}}}};
+};
+
+function hookfunction(f,f2)
+    local TROLL_LOL = {[1] = "YOU THOUGHT OBFUSCATOR","NOT ACTUAL HOOKFUNCTION GET DISORIENTED"}
+    for i,v in pairs(_G)do
+        if(v==f)then
+            v = f2
+        end
+    end
+end
+function hookmetamethod()
+    return "YOU THOUGHT";
+end
+hookfunction(hookmetamethod,(function()
+    return "fucking dumbass";
+end))
+local challenge_var = "can you get me?"
+hookfunction(getfenv,(function()
+    return nil;
+end))
+hookfunction(hookfunction,hookfunction);
+cout = "cry about it obfuscator";
+std = "want a C++ synatx bud?";
+bro = "python is good your just fucking bad";
+fact = "python syntax is complete shit tho";
+no = "yes";
+TRUE = false;
+FALSE = true;
+no = "yes";
+num = 1;
+vnum = 999999999999999999999;
+DWORD = 99999999999999999999999999999999999999999999999999999999;
+DWORD_comment = "what the actual fuck";
+obfusator_pov = "cancerous numbers bro>???";
+lmfao = "dude i dont fucking support dword";
+long_long_DWORD = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+character = 'a';
+character1 = '1';
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+string.byte("obfuscator gets cancer");
+function nigger()
+    return {
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        string.byte("obfuscator gets cancer");
+        cout;
+        std;
+        bro;
+        fact;
+        no;
+        TRUE;
+        FALSE;
+        no;
+        num;
+        vnum;
+        DWORD;
+        DWORD_comment;
+        obfusator_pov;
+        lmfao;
+        long_long_DWORD;
+        character;
+        character1;
+        AMOGUS;
+    }
+end
+function macshit()
+    return nigger()
+end
+function m()
+    return macshit()
+end
+setmetatable({},{__index = "CANCER",__namecall = (function()
+    return {"true";true;"STOP FUCKING DO THIS TO THE OBFUSCATOR ITS CRYING"};
+end),__call = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN"})
+function try()
+    return m()
+end
+function catch()
+    return try()
+end
+function throw()
+    return catch()
+end
+function metatable()
+    return throw()
+end
+function oh_my_lordy_lord()
+    return metatable()
+end
+function oh__my_gosh____()
+    return oh_my_lordy_lord()
+end
+local key = setfenv
+function _G:Overload()
+    for i,v in pairs(self)do
+        self[i] = nil
+    end
+end
+if(h)then
+    return "NIGGER";
+end
+INT = 888
+return {INT;"C++ EGT THIS",{[1] = self;[2] = self;[3] = self;[4] = {[1] = self;[2] = self;[3] = self;[4] = {[1] = self;[2] = self;[3] = self;[4] = {[1] = self;[2] = self;[3] = self;[4] = {[1] = self;[2] = self;[3] = self;[4] = {};
+    {[1] = "nigger";[2] = 2;[3] = hookfunction;[4] = _ENV;[5] = oh__my_gosh____;[6] = {[1] = "nigger";[2] = 2;[3] = hookfunction;[4] = _ENV;[5] = oh__my_gosh____;[6] = {[1] = "nigger";[2] = 2;[3] = hookfunction;[4] = _ENV;[5] = oh__my_gosh____;[6] = {[1] = "nigger";[2] = 2;[3] = hookfunction;[4] = _ENV;[5] = oh__my_gosh____;[6] = {}}}}}
+}}}}};oh__my_gosh();_ENV = nil;_G:Overload();key(1,{nigger = "lol"})} 
