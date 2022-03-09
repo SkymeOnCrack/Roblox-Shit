76221fccccc9eafa3d98f60791812e4c14aa3b01
LOOF-sys <59900863+LOOF-sys@users.noreply.github.com>
2022-03-08 18:07:05 -0600

    Update Versions.lua

--- Versions.lua
+++ Versions.lua
@@ -2,7 +2,7 @@ return {
   Discord = "https://discord.gg/3WJnU7tCGv",
   SharkBite = 4,
   LegendsOfSpeed = 2,
-  MuscleLegends = 4,
+  MuscleLegends = 5,
   ganguponpeoplesimulator = 1,
   MuscleLegendsV2 = 6,
 }
